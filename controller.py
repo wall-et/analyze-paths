753b458ba645ef848f4a14c26e2452bc9a274fd7
Yael Yazdi <yael.yazdi96@gmail.com>
2018-12-12 11:36:56 +0200

    start model

--- controller.py
+++ controller.py
@@ -4,5 +4,5 @@ import model as m
 
 DEFUALT_IMAGE_FILE = "data/fixed.csv"
 
-file =  sys.argv[1] if sys.argv and len(sys.argv) > 1 else DEFUALT_DATA_FILE
+file =  sys.argv[1] if sys.argv and len(sys.argv) > 1 else DEFUALT_IMAGE_FILE
 m.fix_corrupted_file(file)
