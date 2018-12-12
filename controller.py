74f1ac6b6ae08d094e0463ee1b99ada2aad787a1
Yael Yazdi <yael.yazdi96@gmail.com>
2018-12-12 12:21:51 +0200

    part 1 add test model

--- controller.py
+++ controller.py
@@ -7,12 +7,11 @@ from settings import DEFUALT_IMAGE_FILE,DEFUALT_DATA_FILE
 class Controller:
     def __init__(self):
         self.file = get_file()
-    file =  sys.argv[1] if sys.argv and len(sys.argv) > 1 else DEFUALT_IMAGE_FILE
+
+    file = sys.argv[1] if sys.argv and len(sys.argv) > 1 else DEFUALT_IMAGE_FILE
     m.fix_corrupted_file(file)
 
 
-<<<<<<< HEAD
 def get_file():
     pass
-=======
->>>>>>> 0f8f31b2f9671f5246b1976c4080413b908ee7fd
+
--- model.py
+++ model.py
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import sys
-from settings import logger, FIXED_FILE_NAME, ERROR_FILE_NAME ,DEFUALT_DATA_FILE
-=======
+
+
 import pickle
 from settings import logger, FIXED_FILE_NAME, ERROR_FILE_NAME, FIXED_FILE_NAME_PICKLE
->>>>>>> 0f8f31b2f9671f5246b1976c4080413b908ee7fd
-
 
 class Model:
     def __init__(self):
