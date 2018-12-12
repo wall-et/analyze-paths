9264bc5ca4076d9d445389c5d9b992c232f30730
Efrat <efratraube@gmail.com>
2018-12-12 12:06:13 +0200

    mdel class

--- model.py
+++ model.py
@@ -1,26 +1,26 @@
 import sys
 from settings import logger, FIXED_FILE_NAME, ERROR_FILE_NAME,DEFUALT_DATA_FILE
 
+class Model:
+    def fix_corrupted_file(self,file_name):
+        logger.debug(f"entering fix_corrupted_file,file_name={file_name}")
+        logger.error(file_name)
+        valid_counter = 0
+        invalid_counter = 0
 
-def fix_corrupted_file(file_name):
-    logger.debug(f"entering fix_corrupted_file,file_name={file_name}")
-    logger.error(file_name)
-    valid_counter = 0
-    invalid_counter = 0
+        # for line in fileinput.input(file_name):
+        with open(file_name, 'r') as datar, open(FIXED_FILE_NAME, "w", encoding="utf-8") as fixedw, open(
+                ERROR_FILE_NAME,
+                "w",
+                encoding="utf-8") as errorw:
+            for line in datar.readlines():
+                if (len(line.split(', ')) == 14):
+                    fixedw.write(line.strip(" "))
+                    valid_counter += 1
+                else:
+                    errorw.write(line.strip(" "))
+                    invalid_counter += 1
 
-    # for line in fileinput.input(file_name):
-    with open(file_name, 'r') as datar, open(FIXED_FILE_NAME, "w", encoding="utf-8") as fixedw, open(
-            ERROR_FILE_NAME,
-            "w",
-            encoding="utf-8") as errorw:
-        for line in datar.readlines():
-            if (len(line.split(', ')) == 14):
-                fixedw.write(line.strip(" "))
-                valid_counter += 1
-            else:
-                errorw.write(line.strip(" "))
-                invalid_counter += 1
-
-    logger.error(f"{valid_counter} valid lines.")
-    logger.error(f"{invalid_counter} corrupted lines.")
-    logger.error("See corrupt_data.csv")
+        logger.error(f"{valid_counter} valid lines.")
+        logger.error(f"{invalid_counter} corrupted lines.")
+        logger.error("See corrupt_data.csv")
