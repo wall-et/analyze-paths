6dcb89a0965b530bccf7ac6a324ffa24b7ea7442
Efrat <efratraube@gmail.com>
2018-12-13 02:04:28 +0200

    filtering by area works

--- model.py
+++ model.py
@@ -186,24 +186,30 @@ class Model:
 
     def get_data(self,filters):
         intersect_series = self.data_by_objs
+        # intersect_data = self.data
+        logger.debug(f"found {len(intersect_series)} routes by intersect_series")
         logger.debug(type(intersect_series))
         if filters['hour']:
             new_series = self.get_routes_be_hour(filters['hour'][0],filters['hour'][1])
-            intersect_series = pd.Series(list(set(intersect_series).intersection(set(new_series))))
+            logger.debug(f"found {len(new_series)} routes by hour")
+            intersect_series = intersect_series[intersect_series.isin(new_series)]
+
         if filters['area']:
             new_series = self.get_routes_by_area(filters['area'][0],filters['area'][1],filters['area'][2],filters['area'][3])
             logger.debug(f"found {len(new_series)} routes by area")
-            intersect_series = pd.Series(list(set(intersect_series).intersection(set(new_series))))
-            logger.debug(type(intersect_series))
-            logger.debug(type(new_series))
-            logger.debug(f"found {len(intersect_series)} routes by intersect_series")
+            # intersect_series = pd.Series(list(set(intersect_series).intersection(set(new_series))))
+            intersect_series = intersect_series[intersect_series.isin(new_series)]
+
         if filters['date']:
             new_series = self.get_routes_be_date(filters['date'][0],filters['date'][1],filters['date'][2])
-            intersect_series = pd.Series(list(set(intersect_series).intersection(set(new_series))))
+            logger.debug(f"found {len(new_series)} routes by date")
+            intersect_series = intersect_series[intersect_series.isin(new_series)]
+
         if filters['block']:
             # new_series = self.get_square_routes()
-            # intersect_series = pd.Series(list(set(intersect_series).intersection(set(new_series))))
+            # intersect_series = intersect_series[intersect_series.isin(new_series)]
             pass
+
         return (self.data,intersect_series)
 
     # def get_route_by_block(self,x_inds,y_inds):
